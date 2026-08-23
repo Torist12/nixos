178769b3738462156f13a45bbb962d56313130d0
will <daudtpennawilliam@gmail.com>
2026-08-23 00:47:39 -0300

    nixos config

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Config do konqi";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.konqi = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.will = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,59 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "will";
+  home.homeDirectory = "/home/will";
+  home.stateVersion = "24.11";
+
+programs.starship = {
+  enable = true;
+  settings = {
+    add_newline = false;
+    format = "[](fg:#5277C3) [$directory](fg:#7EBAE4)$git_branch [$user](fg:#A6E3A1) >";
+    
+    directory = {
+      format = "$path";
+      truncation_length = 2;
+      truncation_symbol = "…/";
+    };
+    
+    git_branch = {
+      format = " [ $branch](fg:#F9E2AF)";
+    };
+    
+    username = {
+      show_always = true;
+      format = "$user";
+    };
+    
+    character = {
+      success_symbol = "";
+      error_symbol = "";
+    };
+  };
+};
+
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      ll = "ls -la";
+      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#konqi";
+      update = "cd /etc/nixos && sudo nix flake update && sudo nixos-rebuild switch --flake /etc/nixos#konqi && cd -";
+};
+  };
+
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_family = "JetBrainsMono Nerd Font";
+      font_size = 12;
+    };
+  };
+
+  home.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+      nerd-fonts.symbols-only
+  ];
+}
